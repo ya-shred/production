2a283c229b50bf3ad239d7b3f5cbeadb13ef7de9
Andrey Evstropov <genesis.andrey@yandex.ru>
2015-10-12 15:27:13 +0300

    поменял конфиг для продакшна

--- config/production.js
+++ config/production.js
@@ -2,6 +2,6 @@ module.exports = {
     githubAppId: process.env.GITHUB_ID,
     githubAppSecret: process.env.GITHUB_SECRET,
     githubAppCallback: process.env.GITHUB_CALLBACK,
-    frontPort: process.env.FRONT_PORT,
+    frontPort: process.env.FRONT_PORT || process.env.PORT,
     dbConnectionUrl: process.env.MONGOLAB_URI
 };
