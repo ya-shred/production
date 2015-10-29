bdc528a961b1292f9fb9d548c746b73e7f125142
Andrey Evstropov <genesis.andrey@yandex.ru>
2015-10-29 13:31:22 +0300

    убрал оптимизацию js

--- prod.js
+++ prod.js
@@ -2,11 +2,11 @@ var webpack = require('webpack');
 var config = require('./webpack.config');
 
 // https://github.com/webpack/docs/wiki/list-of-plugins#uglifyjsplugin
-config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-        warnings: false
-    }
-}));
+//config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+//    compress: {
+//        warnings: false
+//    }
+//}));
 
 // https://github.com/webpack/docs/wiki/list-of-plugins#noerrorsplugin
 config.plugins.push(new webpack.NoErrorsPlugin());
