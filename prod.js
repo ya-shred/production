a8a0765c87dbbdb2226e0998175cd39bbb8d6211
Andrey Evstropov <genesis.andrey@yandex.ru>
2015-10-29 14:08:47 +0300

    убрал noerrorsplugin

--- prod.js
+++ prod.js
@@ -9,7 +9,7 @@ var config = require('./webpack.config');
 //}));
 
 // https://github.com/webpack/docs/wiki/list-of-plugins#noerrorsplugin
-config.plugins.push(new webpack.NoErrorsPlugin());
+//config.plugins.push(new webpack.NoErrorsPlugin());
 
 var compiler = webpack(config);
 
